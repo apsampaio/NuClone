f9b0b24c4f40e3a99a00174b6b6dd25a4ddc3e66
apsampaio <andre03.sampaio@gmail.com>
2020-04-03 22:06:31 -0300

    firstContent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./global.css";
+
 import Header from "./components/Header";
+import FirstSection from "./components/FirstSection";
 
 export default function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <FirstSection />
+    </>
+  );
 }
